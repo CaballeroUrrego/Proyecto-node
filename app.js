0e478689516123ebe8bc735893bc867b5918f2fe
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-07-02 09:06:06 -0500

    Psotman funcionando

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const { route: productoRoute } = require("./routes/productoRoutes");
 const { usuarioRoute } = require("./routes/usuarioRoutes");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url.startsWith("/api/usuarios")) {
+  // Servir archivos estáticos de /public si la URL termina en .html
+  if (req.url.endsWith(".html")) {
+    const filePath = path.join(__dirname, "public", req.url);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Archivo no encontrado");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+    return;
+  }
+
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Conectado a servidor privado caballero Urrego ");
+  } else if (req.url.startsWith("/api/usuarios")) {
     usuarioRoute(req, res);
   } else {
     productoRoute(req, res);
